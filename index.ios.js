94c1d27faeb4a5571f7e837b17615a4c1cc2ddb2
Pontus <32733993+Pontus1979@users.noreply.github.com>
2018-01-03 17:00:05 +0100

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,13 @@
+//Import a libary to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+//Create a component
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
+
+//Render it to the device
+AppRegistry.registerComponent('albums', () => App);
